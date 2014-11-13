Plain-Andy/android_device_samsung_apexqtmo

Preliminary Aroma installer device overlay

aroma/aroma.mk
@@ -1,0 +1,8 @@
+PRODUCT_COPY_FILES += \
+	device/samsung/apexqtmo/aroma/aroma-device-config:aroma-device-config \
+	device/samsung/apexqtmo/aroma/aroma-device-config:aroma-device-script
+
+#PRODUCT_COPY_FILES += \
+#	device/samsung/apexqtmo/aroma/aroma_device_files/<SOURCE-FILE>:aroma_device/<DEST-FILE>
+## --- If any custom scripting please place in aroma_device dir and call on it in aroma-device-script
+## --- Squisher will retain any directory structure

plain.mk
@@ -9,6 +9,9 @@ $(call inherit-product, vendor/plain/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/plain/config/nfc_enhanced.mk)
 
+# Aroma installer device overlay
+$(call inherit-product, device/samsung/apexqtmo/aroma/aroma.mk)
+
 PRODUCT_NAME := plain_apexqtmo
 PRODUCT_DEVICE := apexqtmo
 PRODUCT_BRAND := samsung

Plain-Andy/android_device_samsung_mondrianwifi

aroma/aroma.mk
@@ -1,0 +1,8 @@
+PRODUCT_COPY_FILES += \
+	device/samsung/mondrianwifi/aroma/aroma-device-config:aroma-device-config \
+	device/samsung/mondrianwifi/aroma/aroma-device-config:aroma-device-script
+
+#PRODUCT_COPY_FILES += \
+#	device/samsung/mondrianwifi/aroma/aroma_device_files/<SOURCE-FILE>:aroma_device/<DEST-FILE>
+## --- If any custom scripting please place in aroma_device dir and call on it in aroma-device-script
+## --- Squisher will retain any directory structure

plain.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
 $(call inherit-product, vendor/plain/config/common.mk)
 
 # Aroma installer device overlay
-#Remove this once finished# $(call inherit-product, device/samsung/mondrianwifi/aroma/aroma.mk)
+$(call inherit-product, device/samsung/mondrianwifi/aroma/aroma.mk)
 
 # Boot animation
 PRODUCT_COPY_FILES += \